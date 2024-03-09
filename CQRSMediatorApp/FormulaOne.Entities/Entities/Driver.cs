6514400c0cb1b3b703fd4ad1120d02714271e150
alex-toy <alexei.80@hotmail.fr>
2024-03-09 12:36:33 +0100

    entities

---
+++ CQRSMediatorApp/FormulaOne.Entities/Entities/Driver.cs
@@ -0,0 +1,16 @@
+﻿namespace FormulaOne.Entities.Entities
+{
+    public class Driver : BaseEntity
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public int DriverNumber { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public ICollection<Achievement> Achievements { get; set; }
+
+        public Driver()
+        {
+            Achievements = new HashSet<Achievement>();
+        }
+    }
+}
